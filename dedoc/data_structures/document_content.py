4c08563784032fbfa93e1aed558e53ab2b978d26
padre <kozlov-ilya@ispras.ru>
2020-07-10 13:40:15 +0300

    add docstring

--- dedoc/data_structures/document_content.py
+++ dedoc/data_structures/document_content.py
@@ -1,13 +1,19 @@
 from typing import List
 from collections import OrderedDict
 
+from dedoc.data_structures.serializable import Serializable
 from dedoc.data_structures.table import Table
 from dedoc.data_structures.tree_node import TreeNode
 
 
-class DocumentContent:
+class DocumentContent(Serializable):
 
     def __init__(self, tables: List[Table], structure: TreeNode):
+        """
+        This class hold document content - structured text and tables
+        :param tables: list of document tables
+        :param structure: document content organized in tree structure
+        """
         self.tables = tables
         self.structure = structure
 
