3cea9717df13961c666d157a11bdae0223a1e0b9
padre <kozlov-ilya@ispras.ru>
2021-01-19 14:29:37 +0300

    RED-10423 add parameters in method can_read for base reader

--- dedoc/readers/reader_composition.py
+++ dedoc/readers/reader_composition.py
@@ -1,4 +1,6 @@
+import inspect
 import os
+import warnings
 from typing import Dict, List
 
 from dedoc.common.exceptions.bad_file_exception import BadFileFormatException
@@ -21,7 +23,22 @@ class ReaderComposition(object):
         document_type = parameters.get("document_type")
 
         for reader in self.readers:
-            if reader.can_read(path=file_path, mime=mime, extension=extension, document_type=document_type):
+            can_read = False
+            if "parameters" in inspect.getfullargspec(reader.can_read).args:
+                can_read = reader.can_read(path=file_path,
+                                           mime=mime,
+                                           extension=extension,
+                                           document_type=document_type,
+                                           parameters=parameters)
+            else:
+                warnings.warn("Please specify parameters argument in method can_read in {}\n"
+                              " This parameters would be mandatory in the near future")
+                can_read = reader.can_read(path=file_path,
+                                           mime=mime,
+                                           extension=extension,
+                                           document_type=document_type,
+                                           parameters=parameters)
+            if can_read:
                 unstructured_document, need_analyze_attachments = reader.read(path=file_path,
                                                                               document_type=document_type,
                                                                               parameters=parameters
