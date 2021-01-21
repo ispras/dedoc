c9ceb659fbced95ed47f9b80a7df3f2c08200217
dronperminov <dronperminov@ispras.ru>
2021-01-21 19:30:42 +0300

    add list patching to structure constructors

--- dedoc/structure_constructor/concreat_structure_constructors/linear_constructor.py
+++ dedoc/structure_constructor/concreat_structure_constructors/linear_constructor.py
@@ -4,6 +4,7 @@ from dedoc.data_structures.document_content import DocumentContent
 from dedoc.data_structures.tree_node import TreeNode
 from dedoc.data_structures.unstructured_document import UnstructuredDocument
 from dedoc.structure_constructor.concreat_structure_constructors.abstract_structure_constructor import AbstractStructureConstructor
+from dedoc.structure_constructor.concreat_structure_constructors.list_patcher import ListPatcher
 
 
 class LinearConstructor(AbstractStructureConstructor):
@@ -14,7 +15,8 @@ class LinearConstructor(AbstractStructureConstructor):
     def structure_document(self,
                            document: UnstructuredDocument,
                            structure_type: Optional[str] = None) -> DocumentContent:
-        lines = document.lines
+        list_patcher = ListPatcher()
+        lines = list_patcher.patch(document.lines)
         tree = TreeNode.create(lines=[])
         for line in lines:
             tree.add_child(line)
