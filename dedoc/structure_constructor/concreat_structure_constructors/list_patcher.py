6ec06902d415ad87a2a48ac7e2b886012355ad4a
dronperminov <dronperminov@ispras.ru>
2021-01-21 19:54:44 +0300

    review fixes

--- dedoc/structure_constructor/concreat_structure_constructors/list_patcher.py
+++ dedoc/structure_constructor/concreat_structure_constructors/list_patcher.py
@@ -10,15 +10,15 @@ class ListPatcher:
         self.list_line_regexp = re.compile(r' *\d+(?:\.\d+|)*.*')
         self.list_item_regexp = re.compile(r' *\d+(?:\.\d+|)*')
 
-    def is_list(self, line: str) -> bool:
+    def __is_list(self, line: str) -> bool:
         return self.list_line_regexp.fullmatch(line) is not None
 
-    def get_list_item(self, line: str) -> List[int]:
+    def __get_list_item(self, line: str) -> List[int]:
         item = self.list_item_regexp.search(line).group(0).lstrip()
         items = [int(list_item) for list_item in item.split(r'.') if list_item]
         return items
 
-    def get_parent(self, list_item: List[int]) -> List[int]:
+    def __get_parent(self, list_item: List[int]) -> List[int]:
         parent = [item for item in list_item]
         parent[-1] -= 1
 
@@ -27,7 +27,7 @@ class ListPatcher:
 
         return parent
 
-    def is_less(self, item1: List[int], item2: List[int]) -> bool:
+    def __is_less(self, item1: List[int], item2: List[int]) -> bool:
         max_len = max(len(item1), len(item2))
 
         for i in range(max_len):
@@ -39,9 +39,9 @@ class ListPatcher:
 
         return False
 
-    def have_parent(self, items: List[List[int]], parent: List[int]) -> int:
+    def __have_parent(self, items: List[List[int]], parent: List[int]) -> int:
         for item in items[::-1]:
-            if self.is_less(parent, item):
+            if self.__is_less(parent, item):
                 return False
 
             if item == parent:
@@ -49,17 +49,17 @@ class ListPatcher:
 
         return False
 
-    def patch_list(self, line: LineWithMeta, items: List[List[int]], levels: List[HierarchyLevel], parent: List[int]):
-        if self.have_parent(items, parent):
-            while self.is_less(items[-1], parent):
+    def __patch_list(self, line: LineWithMeta, items: List[List[int]], levels: List[HierarchyLevel], parent: List[int]):
+        if self.__have_parent(items, parent):
+            while self.__is_less(items[-1], parent):
                 levels.pop()
                 items.pop()
 
         inserting = []
 
-        while parent != items[-1] and self.is_less(items[-1], parent):
+        while parent != items[-1] and self.__is_less(items[-1], parent):
             inserting.append(parent)
-            parent = self.get_parent(parent)
+            parent = self.__get_parent(parent)
 
         patched_lines = []
 
@@ -87,15 +87,15 @@ class ListPatcher:
         patched_lines = []
 
         for line in lines:
-            if not self.is_list(line.line):
+            if not self.__is_list(line.line):
                 patched_lines.append(line)
                 continue
 
-            item = self.get_list_item(line.line)
-            parent = self.get_parent(item)
+            item = self.__get_list_item(line.line)
+            parent = self.__get_parent(item)
 
             if items and parent != items[-1]:
-                patched_lines.extend(self.patch_list(line, items, levels, parent))
+                patched_lines.extend(self.__patch_list(line, items, levels, parent))
 
             items.append(item)
             levels.append(line.hierarchy_level)
