795c5e44ea48939ac7bf94e8173209d3f3ccb6db
Andrew Perminov <dronperminov@gmail.com>
2021-02-16 13:49:17 +0300

    RED-9956 unfix missed lists (#48)
    * RED-9956 unfix missed lists
    
    * RED-9956 remove unnecessary test for list patcher
    
    Co-authored-by: dronperminov <dronperminov@ispras.ru>

--- dedoc/structure_constructor/concreat_structure_constructors/list_patcher.py
+++ dedoc/structure_constructor/concreat_structure_constructors/list_patcher.py
@@ -2,7 +2,6 @@ import re
 from typing import List
 from dedoc.data_structures.line_with_meta import LineWithMeta
 from dedoc.data_structures.line_with_meta import HierarchyLevel
-from dedoc.data_structures.line_with_meta import ParagraphMetadata
 from dedoc.structure_constructor.concreat_structure_constructors.list_item import ListItem
 
 
@@ -18,65 +17,6 @@ class ListPatcher:
         list_item = self.list_item_regexp.search(line).group(0).lstrip()
         items = [int(item) for item in list_item[:-1].split(r'.') if item]
         return ListItem(items, list_item[-1:])
-
-    def __have_parent(self, items: List[ListItem], parent: ListItem) -> int:
-        for item in items[::-1]:
-            if parent == item:
-                return True
-
-        return False
-
-    def __have_type(self, items: List[ListItem], end_type: str) -> bool:
-        for item in items[::-1]:
-            if item.end_type == end_type:
-                return True
-
-        return False
-
-    def __get_inserting_parents(self, parent: ListItem, items: List[ListItem], levels: List[HierarchyLevel]) -> List[ListItem]:
-        if self.__have_parent(items, parent):
-            while items[-1] != parent:
-                levels.pop()
-                items.pop()
-
-        if self.__have_type(items, parent.end_type):
-            while parent.end_type != items[-1].end_type:
-                items.pop()
-                levels.pop()
-
-        inserting = []
-
-        while parent != items[-1] and items[-1] < parent:
-            inserting.append(parent)
-            parent = parent.get_parent()
-
-        return inserting
-
-    def __patch_list(self, line: LineWithMeta, items: List[ListItem], levels: List[HierarchyLevel], parent: ListItem):
-        inserting = self.__get_inserting_parents(parent, items, levels)
-        patched_lines = []
-
-        while inserting:
-            last = inserting.pop()
-            item_str = str(last)
-            paragraph_type = HierarchyLevel.raw_text if levels[-1].level_1 is None else HierarchyLevel.list_item
-            hierarchy_level = HierarchyLevel(level_1=levels[-1].level_1,
-                                             level_2=len(last.item),
-                                             paragraph_type=paragraph_type,
-                                             can_be_multiline=False)
-
-            patched_lines.append(LineWithMeta(line=item_str,
-                                              hierarchy_level=hierarchy_level,
-                                              metadata=ParagraphMetadata(paragraph_type="list_item",
-                                                                         page_id=line.metadata.page_id,
-                                                                         line_id=line.metadata.line_id,
-                                                                         predicted_classes=None),
-                                              annotations=[]))
-
-            items.append(last)
-            levels.append(hierarchy_level)
-
-        return patched_lines
 
     def __update_line_levels(self, lines: List[LineWithMeta], list_item_line: LineWithMeta):
         for line in lines:
@@ -102,13 +42,9 @@ class ListPatcher:
                 continue
 
             item = self.__get_list_item(line.line)
-            parent = item.get_parent()
 
             if not item.is_first_item() and items:
                 self.__update_line_levels(content, line)
-
-                if parent != items[-1]:
-                    patched_lines.extend(self.__patch_list(line, items, levels, parent))
 
             patched_lines.extend(content)
             content = []
--- tests/api_tests/test_api_list_patching.py
+++ tests/api_tests/test_api_list_patching.py
@@ -9,8 +9,7 @@ class TestApiDocReader(AbstractTestApiDocReader):
         content = result["content"]["structure"]
 
         lst = content["subparagraphs"][2]
-        self.assertEqual(len(lst["subparagraphs"][0]["subparagraphs"]), 6)
-        self.assertEqual(lst["subparagraphs"][0]["subparagraphs"][2]["text"], "2.3.")
+        self.assertEqual(len(lst["subparagraphs"][0]["subparagraphs"]), 5)
 
     def test_list_patching_2(self):
         file_name = "list_tests/missed_list.docx"
@@ -18,33 +17,24 @@ class TestApiDocReader(AbstractTestApiDocReader):
         content = result["content"]["structure"]
 
         list = content["subparagraphs"][0]
-        self.assertEqual(len(list["subparagraphs"]), 6)
+        self.assertEqual(len(list["subparagraphs"]), 5)
         self.assertEqual(list["subparagraphs"][0]["text"], "1. list item 1")
         self.assertEqual(list["subparagraphs"][1]["text"], "2. list item 2")
         self.assertEqual(list["subparagraphs"][2]["text"], "3. list item 3")
         self.assertEqual(list["subparagraphs"][3]["text"], "4. list item 4")
-        self.assertEqual(list["subparagraphs"][4]["text"], "5.")
-        self.assertEqual(list["subparagraphs"][5]["text"], "6. list item 6")
+        self.assertEqual(list["subparagraphs"][4]["text"], "6. list item 6")
 
-        list = list["subparagraphs"][5]["subparagraphs"][0]
-        self.assertEqual(len(list["subparagraphs"]), 5)
+        list = list["subparagraphs"][4]["subparagraphs"][0]
+        self.assertEqual(len(list["subparagraphs"]), 3)
         self.assertEqual(list["subparagraphs"][0]["text"], "6.1. list item 6.1")
-        self.assertEqual(list["subparagraphs"][1]["text"], "6.2.")
-        self.assertEqual(list["subparagraphs"][2]["text"], "6.3 list item 6.3")
-        self.assertEqual(list["subparagraphs"][3]["text"], "6.4.")
-        self.assertEqual(list["subparagraphs"][4]["text"], "6.5 list item 6.5")
+        self.assertEqual(list["subparagraphs"][1]["text"], "6.3 list item 6.3")
+        self.assertEqual(list["subparagraphs"][2]["text"], "6.5 list item 6.5")
 
-        list = list["subparagraphs"][2]["subparagraphs"][0]
+        list = list["subparagraphs"][1]["subparagraphs"][1]
+
         self.assertEqual(len(list["subparagraphs"]), 2)
-        self.assertEqual(list["subparagraphs"][0]["text"], "6.3.1.")
-        self.assertEqual(list["subparagraphs"][1]["text"], "6.3.2.")
-
-        list = list["subparagraphs"][1]["subparagraphs"][0]
-        self.assertEqual(len(list["subparagraphs"]), 4)
-        self.assertEqual(list["subparagraphs"][0]["text"], "6.3.2.1.")
-        self.assertEqual(list["subparagraphs"][1]["text"], "6.3.2.2.")
-        self.assertEqual(list["subparagraphs"][2]["text"], "6.3.2.3 list item 6.3.2.3")
-        self.assertEqual(list["subparagraphs"][3]["text"], "6.3.2.4. list item 6.3.2.4")
+        self.assertEqual(list["subparagraphs"][0]["text"], "6.3.2.3 list item 6.3.2.3")
+        self.assertEqual(list["subparagraphs"][1]["text"], "6.3.2.4. list item 6.3.2.4")
 
     def test_list_patching_3(self):
         file_name = "list_tests/missed_list_2.docx"
@@ -52,38 +42,28 @@ class TestApiDocReader(AbstractTestApiDocReader):
         content = result["content"]["structure"]
 
         list = content["subparagraphs"][0]
-        self.assertEqual(len(list["subparagraphs"]), 6)
+        self.assertEqual(len(list["subparagraphs"]), 5)
         self.assertEqual(list["subparagraphs"][0]["text"], "1. list item 1")
         self.assertEqual(list["subparagraphs"][1]["text"], "2. list item 2")
         self.assertEqual(list["subparagraphs"][2]["text"], "3. list item 3")
         self.assertEqual(list["subparagraphs"][3]["text"], "4. list item 4")
-        self.assertEqual(list["subparagraphs"][4]["text"], "5.")
-        self.assertEqual(list["subparagraphs"][5]["text"], "6. list item 6")
+        self.assertEqual(list["subparagraphs"][4]["text"], "6. list item 6")
 
         self.assertEqual(list["subparagraphs"][1]["subparagraphs"][0]["subparagraphs"][0]["text"], "1)")
-        self.assertEqual(list["subparagraphs"][1]["subparagraphs"][0]["subparagraphs"][1]["text"], "2)")
-        self.assertEqual(list["subparagraphs"][1]["subparagraphs"][0]["subparagraphs"][2]["text"], "3)")
-        self.assertEqual(list["subparagraphs"][1]["subparagraphs"][0]["subparagraphs"][3]["text"], "4)")
-        self.assertEqual(list["subparagraphs"][1]["subparagraphs"][0]["subparagraphs"][4]["text"], "5)")
-        self.assertEqual(list["subparagraphs"][1]["subparagraphs"][0]["subparagraphs"][5]["text"], "6)")
-        self.assertEqual(list["subparagraphs"][1]["subparagraphs"][0]["subparagraphs"][6]["text"], "7)")
+        self.assertEqual(list["subparagraphs"][1]["subparagraphs"][0]["subparagraphs"][1]["text"], "3)")
+        self.assertEqual(list["subparagraphs"][1]["subparagraphs"][0]["subparagraphs"][2]["text"], "4)")
+        self.assertEqual(list["subparagraphs"][1]["subparagraphs"][0]["subparagraphs"][3]["text"], "7)")
 
-        list = list["subparagraphs"][5]["subparagraphs"][0]
-        self.assertEqual(len(list["subparagraphs"]), 5)
+        list = list["subparagraphs"][4]["subparagraphs"][0]
+        self.assertEqual(len(list["subparagraphs"]), 3)
         self.assertEqual(list["subparagraphs"][0]["text"], "6.1. list item 6.1")
-        self.assertEqual(list["subparagraphs"][1]["text"], "6.2.")
-        self.assertEqual(list["subparagraphs"][2]["text"], "6.3 list item 6.3")
-        self.assertEqual(list["subparagraphs"][3]["text"], "6.4.")
-        self.assertEqual(list["subparagraphs"][4]["text"], "6.5 list item 6.5")
+        # self.assertEqual(list["subparagraphs"][1]["text"], "6.2.")
+        self.assertEqual(list["subparagraphs"][1]["text"], "6.3 list item 6.3")
+        # self.assertEqual(list["subparagraphs"][3]["text"], "6.4.")
+        self.assertEqual(list["subparagraphs"][2]["text"], "6.5 list item 6.5")
 
-        list = list["subparagraphs"][2]["subparagraphs"][0]
+        list = list["subparagraphs"][1]["subparagraphs"][1]
+
         self.assertEqual(len(list["subparagraphs"]), 2)
-        self.assertEqual(list["subparagraphs"][0]["text"], "6.3.1.")
-        self.assertEqual(list["subparagraphs"][1]["text"], "6.3.2.")
-
-        list = list["subparagraphs"][1]["subparagraphs"][0]
-        self.assertEqual(len(list["subparagraphs"]), 4)
-        self.assertEqual(list["subparagraphs"][0]["text"], "6.3.2.1.")
-        self.assertEqual(list["subparagraphs"][1]["text"], "6.3.2.2.")
-        self.assertEqual(list["subparagraphs"][2]["text"], "6.3.2.3 list item 6.3.2.3")
-        self.assertEqual(list["subparagraphs"][3]["text"], "6.3.2.4. list item 6.3.2.4")
+        self.assertEqual(list["subparagraphs"][0]["text"], "6.3.2.3. list item 6.3.2.3")
+        self.assertEqual(list["subparagraphs"][1]["text"], "6.3.2.4. list item 6.3.2.4")
