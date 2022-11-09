5ebc7358d86cc8d025342a4a342b2e642486b7ed
Belyaeva Oksana <belyaeva@ispras.ru>
2022-11-09 16:03:58 +0300

    fixed tests

---
+++ dedoc/tests/units/test_ppt_converter.py
@@ -0,0 +1,27 @@
+import os
+
+from dedoc.common.exceptions.conversion_exception import ConversionException
+from dedoc.converters.concrete_converters.pptx_converter import PptxConverter
+from dedoc.tests.units.abstract_converter_test import AbstractConverterTest
+
+
+class TestDocxConverter(AbstractConverterTest):
+
+    path = os.path.join(AbstractConverterTest.path, "pptx")
+    converter = PptxConverter(config={"need_content_analysis": True})
+
+    def test_convert_broken(self) -> None:
+        extension = ".odp"
+        filename = "broken"
+        with self.assertRaises(ConversionException):
+            self._convert(filename=filename, extension=extension, converter=self.converter)
+
+    def test_convert_odp(self) -> None:
+        filename = "example"
+        extension = ".odp"
+        self._convert(filename=filename, extension=extension, converter=self.converter)
+
+    def test_convert_ppt(self) -> None:
+        filename = "example"
+        extension = ".ppt"
+        self._convert(filename=filename, extension=extension, converter=self.converter)
