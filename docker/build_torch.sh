a43a365ed7f72e3909a045d941d8b919923d4875
Belyaeva Oksana <belyaeva@ispras.ru>
2022-12-19 17:25:16 +0300

    some fixes

--- docker/build_torch.sh
+++ docker/build_torch.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 # build secure pytorch 1.11.0 (>o<) without CUDA
 pip3.8 install cmake --upgrade
-bash Miniconda3-py38_4.12.0-Linux-x86_64.sh -b -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
 ~/miniconda3/bin/conda create --name pytorch-build python=3.8
 ~/miniconda3/bin/conda activate pytorch-build
 ~/miniconda3/bin/conda install astunparse numpy ninja pyyaml mkl mkl-include setuptools cmake cffi typing_extensions future six requests dataclasses
