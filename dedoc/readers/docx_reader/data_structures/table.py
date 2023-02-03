01065380eaf198c84749c21fa1d915717d1851a6
Beka00best <bukhatovb@ispras.ru>
2023-02-03 14:35:19 +0300

    refactoring DocxTable.to_table and add test with big table

--- dedoc/readers/docx_reader/data_structures/table.py
+++ dedoc/readers/docx_reader/data_structures/table.py
@@ -1,10 +1,13 @@
 import hashlib
-from types import SimpleNamespace
-from typing import List, Tuple
+from collections import namedtuple
 from bs4 import BeautifulSoup
 
+from dedoc.data_structures.table import Table
+from dedoc.data_structures.table_metadata import TableMetadata
 from dedoc.readers.docx_reader.data_structures.run import Run
 from dedoc.readers.docx_reader.styles_extractor import StylesExtractor
+
+CellPropertyInfo = namedtuple('NamedTuple', 'colspan, rowspan, invisible')
 
 
 class DocxTable:
@@ -23,10 +26,9 @@ class DocxTable:
     def uid(self) -> str:
         return self._uid
 
-    def get_cells(self) -> Tuple[List[List[str]], List[List[SimpleNamespace]]]:
+    def to_table(self) -> Table:
         """
-        returns list of lists with table cells
-        merged cells are split and duplicated in result table
+        returns table class
         """
         # tbl tag defines table
         # tr tag defines table row
@@ -41,42 +43,39 @@ class DocxTable:
         prev_row = []
 
         cell_property_list = []
-        for row in rows:
+        rowspan_start_info = {}
+        for row_index, row in enumerate(rows):
             cells = row.find_all("w:tc")
             cells_text = []
 
             cell_ind = 0
             cell_property_row_list = []
             for cell in cells:
-                cell_property_info = {}
                 # gridSpan tag describes number of horizontally merged cells
                 if cell.gridSpan:
                     grid_span = int(cell.gridSpan["w:val"])
                 else:
                     grid_span = 1
-                cell_property_info["colspan"] = grid_span
                 # get text of the cell
                 cell_text = self.__get_cell_text(cell)
                 # vmerge tag for vertically merged set of cells (or horizontally split cells)
                 # attribute val may be "restart" or "continue" ("continue" if omitted)
-                invisible_flag = False
                 if cell.vMerge:
                     value = cell.vMerge.get("w:val", "continue")
                     if value == "continue":
-                        invisible_flag = True
+                        cell_property_row_list.append(CellPropertyInfo(1, 1, True))
                         cell_text += prev_row[cell_ind]
-                    cell_property_info["rowspan"] = 0
+                        last_cell_rowspan = cell_property_list[rowspan_start_info[cell_ind]][cell_ind]
+                        cell_property_list[rowspan_start_info[cell_ind]][cell_ind] = last_cell_rowspan._replace(
+                            rowspan=last_cell_rowspan.rowspan + 1)
+                    elif value == "restart":
+                        rowspan_start_info[cell_ind] = row_index
+                        cell_property_row_list.append(CellPropertyInfo(grid_span, 1, False))
                 else:
-                    cell_property_info["rowspan"] = 1
-                cell_property_info["invisible"] = invisible_flag
+                    cell_property_row_list.append(CellPropertyInfo(grid_span, 1, False))
                 # split merged cells
-                cell_property_row_list.append(SimpleNamespace(**cell_property_info))
-                cell_property_info = {}
                 for span in range(grid_span - 1):
-                    cell_property_info["colspan"] = 1
-                    cell_property_info["rowspan"] = 1
-                    cell_property_info["invisible"] = True
-                    cell_property_row_list.append(SimpleNamespace(**cell_property_info))
+                    cell_property_row_list.append(CellPropertyInfo(1, 1, True))
                     cell_ind += 1
                     cells_text.append(cell_text)
                 cell_ind += 1
@@ -84,8 +83,8 @@ class DocxTable:
             cell_property_list.append(cell_property_row_list)
             result_cells.append(cells_text)
             prev_row = cells_text
-        cell_property_list = self.__corrected_rowspan_list(cell_property_list)
-        return result_cells, cell_property_list
+        return Table(cells=result_cells, metadata=TableMetadata(page_id=None, uid=self.uid),
+                     cells_with_property=cell_property_list)
 
     def __get_cell_text(self, cell: BeautifulSoup) -> str:
         cell_text = ""
@@ -99,20 +98,3 @@ class DocxTable:
         if cell_text:
             cell_text = cell_text[:-1]  # remove \n in the end
         return cell_text
-
-    @staticmethod
-    def __corrected_rowspan_list(cell_property_list: List[List[SimpleNamespace]]) -> List[List[SimpleNamespace]]:
-        for col_index in range(len(cell_property_list[0])):
-            rowspan_start_index = 0
-            rowspan_count = 0
-            for row_index, row in enumerate(cell_property_list):
-                cell = cell_property_list[row_index][col_index]
-                if cell.rowspan == 0:
-                    rowspan_count += 1
-                    if cell.invisible is False:
-                        rowspan_start_index = row_index
-                    elif cell.invisible is True:
-                        cell.rowspan = 1
-            if rowspan_count != 0:
-                cell_property_list[rowspan_start_index][col_index].rowspan = rowspan_count
-        return cell_property_list
