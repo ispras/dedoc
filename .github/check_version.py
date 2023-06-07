7f3f1a279934820bebcc39e23acc7f52fae06e4a
Bogatenkova Anastasiya <bogatenkova.anastasiya@mail.ru>
2023-06-07 16:52:08 +0300

    TLDR-350 pypi pipeline fix (#277)
    * Check version before push to pypi
    
    * Change version (incorrect)
    
    * Check master pattern
    
    * Add more checks for develop version
    
    * Incorrect version again
    
    * Check develop pattern
    
    * Remove testing artifacts

---
+++ .github/check_version.py
@@ -0,0 +1,43 @@
+import argparse
+import re
+from typing import Pattern
+
+
+def is_correct_version(version: str, old_version: str, regexp: Pattern) -> bool:
+    match = regexp.match(version)
+
+    if match is None:
+        print("New version doesn't match the pattern")  # noqa
+        return False
+
+    return old_version < version
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--branch", help="Git branch to check its version", choices=["develop", "master"])
+    parser.add_argument("--new_version", help="Version on this branch", type=str)
+    parser.add_argument("--old_version", help="Previous version on this branch", type=str)
+    args = parser.parse_args()
+
+    print(f"Old version: {args.old_version}, new version: {args.new_version}")  # noqa
+
+    master_version_pattern = re.compile(r"^\d+\.\d+(\.\d+)?$")
+    develop_version_pattern = re.compile(r"^\d+\.\d+\.\d+rc\d+$")
+
+    correct = False
+    if args.branch == "develop":
+        correct = is_correct_version(args.new_version, args.old_version, develop_version_pattern)
+
+        if correct and master_version_pattern.match(args.old_version) and args.new_version.split("rc")[0] <= args.old_version:
+            correct = False
+            print("New version should add 'rc' to the bigger version than the old one")  # noqa
+        elif correct and int(args.new_version.split("rc")[1]) == 0:
+            correct = False
+            print("Numeration for 'rc' should start from 1")  # noqa
+
+    if args.branch == "master":
+        correct = is_correct_version(args.new_version, args.old_version, master_version_pattern)
+
+    assert correct
+    print("Version is correct")  # noqa
