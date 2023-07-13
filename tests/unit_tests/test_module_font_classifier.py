80ac1a901f72820f59af77a00330c65223d7da6f
Alexander Golodkov <golodkov@ispras.ru>
2023-07-13 17:54:14 +0300

    fixed test for font classifier

--- tests/unit_tests/test_module_font_classifier.py
+++ tests/unit_tests/test_module_font_classifier.py
@@ -3,41 +3,44 @@ import unittest
 
 import cv2
 
+from dedoc.data_structures import BoldAnnotation
 from dedoc.data_structures.bbox import BBox
 from dedoc.readers.pdf_reader.data_classes.page_with_bboxes import PageWithBBox
 from dedoc.readers.pdf_reader.data_classes.text_with_bbox import TextWithBBox
 from dedoc.readers.pdf_reader.pdf_image_reader.line_metadata_extractor.font_type_classifier import FontTypeClassifier
-from tests.test_utils import get_test_config
 
 
 class TestFontClassifier(unittest.TestCase):
     data_directory_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data", "scanned"))
-    path_model = os.path.abspath(os.path.join(get_test_config()["resources_path"], "font_classifier.pth"))
-    classifier = FontTypeClassifier(path_model)
+    classifier = FontTypeClassifier()
 
-    def get_page_with_bbox(self, filename) -> PageWithBBox:
-        image = cv2.imread(os.path.join(self.data_directory_path, filename))
+    def get_page(self) -> PageWithBBox:
+        image = cv2.imread(os.path.join(self.data_directory_path, "example.png"))
 
-        bbox_1 = TextWithBBox(bbox=BBox(10, 20, 11, 23), page_num=0, text="str", line_num=0)
-        bbox_2 = TextWithBBox(bbox=BBox(20, 30, 11, 23), page_num=0, text="rts", line_num=1)
-        bboxes = [bbox_1, bbox_2]
+        bboxes = [
+            TextWithBBox(bbox=BBox(79, 86, 214, 21), page_num=0, text="Пример документа", line_num=0),
+            TextWithBBox(bbox=BBox(79, 113, 627, 20), page_num=0, text="Глава 1 с таким длинным названием которое даже не влазит в", line_num=0),
+            TextWithBBox(bbox=BBox(80, 142, 132, 16), page_num=0, text="одну строчку.", line_num=0),
+            TextWithBBox(bbox=BBox(80, 163, 154, 15), page_num=0, text="Какие то определения", line_num=0),
+            TextWithBBox(bbox=BBox(79, 182, 65, 11), page_num=0, text="Статья 1", line_num=0),
+            TextWithBBox(bbox=BBox(79, 201, 166, 15), page_num=0, text="опрделения", line_num=0),
+            TextWithBBox(bbox=BBox(79, 220, 66, 11), page_num=0, text="Статья 2", line_num=0),
+            TextWithBBox(bbox=BBox(79, 239, 124, 14), page_num=0, text="Дадим пояснения", line_num=0),
+            TextWithBBox(bbox=BBox(81, 259, 203, 11), page_num=0, text="1.2.1 Поясним за непонятное", line_num=0),
+            TextWithBBox(bbox=BBox(81, 278, 191, 11), page_num=0, text="1.2.2. Поясним за понятное", line_num=0),
+            TextWithBBox(bbox=BBox(129, 297, 171, 15), page_num=0, text="а) это даже ежу понятно", line_num=0),
+            TextWithBBox(bbox=BBox(129, 315, 153, 16), page_num=0, text="6) это ежу не понятно", line_num=0),
+            TextWithBBox(bbox=BBox(81, 335, 30, 11), page_num=0, text="123", line_num=0),
+        ]
 
         return PageWithBBox(image=image, bboxes=bboxes, page_num=0)
 
-    def test_page_with_bbox_converted_to_tensor(self) -> None:
-        """
-        Tests font classifier output tensor shape correctness
-        """
-        page = self.get_page_with_bbox(filename="orient_1.png")
-        tensor = FontTypeClassifier._page2tensor(page=page)
-        bbox_num, channels, height, width = tensor.shape
-        self.assertEqual(2, bbox_num)
-        self.assertEqual(3, channels)
-        self.assertEqual(15, height)
-        self.assertEqual(300, width)
+    def test_bold_classification(self) -> None:
+        page = self.get_page()
+        self.classifier.predict_annotations(page)
 
-    def test_get_model_predictions(self) -> None:
-        page = self.get_page_with_bbox(filename="orient_1.png")
-        predictions = self.classifier._get_model_predictions(page)
-        self.assertEqual(predictions.shape[0], 2)
-        self.assertEqual(len(predictions.shape), 2)
+        for bbox in page.bboxes[:3]:
+            self.assertIn(BoldAnnotation.name, [annotation.name for annotation in bbox.annotations])
+
+        for bbox in page.bboxes[3:]:
+            self.assertNotIn(BoldAnnotation.name, [annotation.name for annotation in bbox.annotations])
