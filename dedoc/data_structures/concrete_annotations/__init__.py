a17c8edd1a7681aa4177275c01eda4d6d01281fd
dronperminov <perminov@ispras.ru>
2023-07-31 14:42:27 +0300

    add new annotation

--- dedoc/data_structures/concrete_annotations/__init__.py
+++ dedoc/data_structures/concrete_annotations/__init__.py
@@ -3,6 +3,7 @@ from .attach_annotation import AttachAnnotation
 from .bbox_annotation import BBoxAnnotation
 from .bold_annotation import BoldAnnotation
 from .color_annotation import ColorAnnotation
+from .confidence_annotation import ConfidenceAnnotation
 from .indentation_annotation import IndentationAnnotation
 from .italic_annotation import ItalicAnnotation
 from .linked_text_annotation import LinkedTextAnnotation
@@ -15,6 +16,7 @@ from .superscript_annotation import SuperscriptAnnotation
 from .table_annotation import TableAnnotation
 from .underlined_annotation import UnderlinedAnnotation
 
-__all__ = ['AlignmentAnnotation', 'AttachAnnotation', 'BBoxAnnotation', 'BoldAnnotation', 'ColorAnnotation', 'IndentationAnnotation',
-           'ItalicAnnotation', 'LinkedTextAnnotation', 'SizeAnnotation', 'SpacingAnnotation', 'StrikeAnnotation', 'StyleAnnotation',
-           'SubscriptAnnotation', 'SuperscriptAnnotation', 'TableAnnotation', 'UnderlinedAnnotation']
+__all__ = [
+    'AlignmentAnnotation', 'AttachAnnotation', 'BBoxAnnotation', 'BoldAnnotation', 'ColorAnnotation', 'ConfidenceAnnotation',
+           'IndentationAnnotation', 'ItalicAnnotation', 'LinkedTextAnnotation', 'SizeAnnotation', 'SpacingAnnotation', 'StrikeAnnotation',
+           'StyleAnnotation', 'SubscriptAnnotation', 'SuperscriptAnnotation', 'TableAnnotation', 'UnderlinedAnnotation']
