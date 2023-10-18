11889b40d8049711468d13a9300d2caca2e15703
Nikita Shevtsov <shevtsov@ispras.ru>
2023-10-18 16:11:10 +0300

    add chaching

--- dedoc/scripts/test_pdf_miner.py
+++ dedoc/scripts/test_pdf_miner.py
@@ -1,30 +1,45 @@
 import json
 import os
 import re
+import zipfile
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-import requests
+import wget
 
 from dedoc.api.api_utils import json2txt
+from dedoc.config import get_config
 from dedoc.dedoc_manager import DedocManager
 
 
 URL = "https://at.ispras.ru/owncloud/index.php/s/uImxYhliBHU8ei7/download"
+URL_GT = "https://at.ispras.ru/owncloud/index.php/s/SXsOTqxGaGO9wL9/download"
 
 if __name__ == "__main__":
-    response = requests.get(URL)
+    data_dir = Path(get_config()["intermediate_data_path"]) /  "text_layer_correctness_data"
+    benchmark_data_dir = data_dir / "benchmark_pdf_miner"
+
+    if not os.path.isdir(benchmark_data_dir):
+        benchmark_data_dir.mkdir(parents=True)
+        wget.download(URL, str(benchmark_data_dir / "pdfs.zip"))
+        wget.download(URL_GT, str(benchmark_data_dir / "pdfs_gt.zip"))
+
+        with zipfile.ZipFile(benchmark_data_dir / "pdfs.zip", 'r') as zip_ref:
+            zip_ref.extractall(benchmark_data_dir)
+        os.remove(benchmark_data_dir / "pdfs.zip")
+        with zipfile.ZipFile(benchmark_data_dir / "pdfs_gt.zip", 'r') as zip_ref:
+            zip_ref.extractall(benchmark_data_dir)
+        os.remove(benchmark_data_dir / "pdfs_gt.zip")
+
+        print(f"Benchmark data downloaded to {benchmark_data_dir}")
+    else:
+        print(f"Use cached benchmark data from {benchmark_data_dir}")
+
+    pdfs_path = benchmark_data_dir / "PdfMiner params "
+    pdfs_gt_path = benchmark_data_dir / "PdfMiner Params GT"
+
     info = dict()
     with TemporaryDirectory() as tmpdir:
-        with (Path(tmpdir) / "pdfs.zip").open("wb") as f:
-            f.write(response.content)
-        import zipfile
-
-        with zipfile.ZipFile((Path(tmpdir) / "pdfs.zip"), "r") as zip_ref:
-            zip_ref.extractall((Path(tmpdir)))
-        os.remove(Path(tmpdir) / "pdfs.zip")
-        pdfs_path = Path(tmpdir) / "PdfMiner params "
-
         manager = DedocManager()
         for file in os.listdir(pdfs_path):
             result = manager.parse(file_path=str(pdfs_path / file), parameters={"pdf_with_text_layer": "true"})
@@ -33,7 +48,7 @@ if __name__ == "__main__":
                 f.write(txt_content)
 
             accuracy_script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "accuracy"))
-            gt_path = Path("pdf_ground_truths") / (file[:-3] + "txt")
+            gt_path = pdfs_gt_path / (file[:-3] + "txt")
             tmp_ocr_path = Path(tmpdir) / "ocr.txt"
             accuracy_path = Path(tmpdir) / "accuracy.txt"
             if accuracy_path.exists():
@@ -51,6 +66,6 @@ if __name__ == "__main__":
 
     output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "resources", "benchmarks"))
     with (Path(output_dir) / "test_pdf_miner.json").open("w") as f:
-        json.dump(info, f)
+        json.dump(info, f, ensure_ascii=False, indent=2)
 
-    print("save result in" + output_dir)
+    print(f"save result in {output_dir}")
