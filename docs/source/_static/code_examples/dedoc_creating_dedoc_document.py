6bc8512a1b2b090d2714ba2124fa2afb2fa09525
Nikita Shevtsov <shevtsov@ispras.ru>
2023-11-20 16:08:15 +0300

    fix style

--- docs/source/_static/code_examples/dedoc_creating_dedoc_document.py
+++ docs/source/_static/code_examples/dedoc_creating_dedoc_document.py
@@ -1,9 +1,8 @@
 import uuid
 
-from dedoc.data_structures import BoldAnnotation, CellWithMeta, HierarchyLevel, LineMetadata, LineWithMeta, \
-    LinkedTextAnnotation, Table, TableMetadata, AttachedFile, UnstructuredDocument, TableAnnotation, AttachAnnotation
+from dedoc.data_structures import AttachAnnotation, AttachedFile, BoldAnnotation, CellWithMeta, HierarchyLevel, LineMetadata, LineWithMeta, \
+    LinkedTextAnnotation, Table, TableAnnotation, TableMetadata, UnstructuredDocument
 from dedoc.structure_constructors import TreeConstructor
-
 
 text = "Simple text line"
 simple_line = LineWithMeta(text)
@@ -38,15 +37,15 @@ table_metadata = TableMetadata(page_id=0, uid="table")
 table = Table(cells=cells_with_meta, metadata=table_metadata)
 
 table_line_metadata = LineMetadata(
-        page_id=0,
-        line_id=None,
-        hierarchy_level=HierarchyLevel(
-            level_1=1,
-            level_2=0,
-            can_be_multiline=False,
-            line_type="raw_text"
-        ),
-    )
+    page_id=0,
+    line_id=None,
+    hierarchy_level=HierarchyLevel(
+        level_1=1,
+        level_2=0,
+        can_be_multiline=False,
+        line_type="raw_text"
+    ),
+)
 table_line = LineWithMeta("Line with simple table", metadata=table_line_metadata, annotations=[TableAnnotation("table", 0, 21)])
 
 table_cells = [["Last name First name Patronymic", "Last name First name Patronymic", "Last name First name Patronymic"],
@@ -69,29 +68,29 @@ table_metadata = TableMetadata(page_id=0, uid="complicated_table")
 complicated_table = Table(cells=cells_with_meta, metadata=table_metadata)
 
 complicated_table_line_metadata = LineMetadata(
-        page_id=0,
-        line_id=None,
-        hierarchy_level=HierarchyLevel(
-            level_1=1,
-            level_2=0,
-            can_be_multiline=False,
-            line_type="raw_text"
-        ),
-    )
+    page_id=0,
+    line_id=None,
+    hierarchy_level=HierarchyLevel(
+        level_1=1,
+        level_2=0,
+        can_be_multiline=False,
+        line_type="raw_text"
+    ),
+)
 complicated_table_line = LineWithMeta("complicated table line", metadata=table_line_metadata, annotations=[TableAnnotation("complicated_table", 0, 21)])
 
 attached_file = AttachedFile(original_name="docx_example.png", tmp_file_path="test_dir/docx_example.png", need_content_analysis=False, uid=str(uuid.uuid4()))
 
 attached_file_line_metadata = LineMetadata(
-        page_id=0,
-        line_id=None,
-        hierarchy_level=HierarchyLevel(
-            level_1=1,
-            level_2=0,
-            can_be_multiline=False,
-            line_type="raw_text"
-        ),
-    )
+    page_id=0,
+    line_id=None,
+    hierarchy_level=HierarchyLevel(
+        level_1=1,
+        level_2=0,
+        can_be_multiline=False,
+        line_type="raw_text"
+    ),
+)
 attached_file_line = LineWithMeta("Line with attached file", metadata=attached_file_line_metadata, annotations=[AttachAnnotation("super table", 0, 21)])
 
 unstructured_document = UnstructuredDocument(
@@ -101,16 +100,16 @@ unstructured_document = UnstructuredDocument(
 )
 
 unstructured_document.metadata = {
-            "file_name": "my_document.txt",
-            "temporary_file_name": "my_document.txt",
-            "file_type": "txt",
-            "size": 11111,  # in bytes
-            "access_time": 1696381364,
-            "created_time": 1696316594,
-            "modified_time": 1696381364
-        }
+    "file_name": "my_document.txt",
+    "temporary_file_name": "my_document.txt",
+    "file_type": "txt",
+    "size": 11111,  # in bytes
+    "access_time": 1696381364,
+    "created_time": 1696316594,
+    "modified_time": 1696381364
+}
 
 structure_constructor = TreeConstructor()
 parsed_document = structure_constructor.structure_document(document=unstructured_document, structure_type="tree")
 
-print(parsed_document.to_api_schema().model_dump())
+parsed_document.to_api_schema().model_dump()
