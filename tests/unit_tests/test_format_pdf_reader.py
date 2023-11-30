a0710d62715ae6aecf9a9607b71b4cbccc34b2ce
raxtemur <raxtemur@gmail.com>
2023-11-30 23:26:15 +0000

    TLDR-531 style fix

--- tests/unit_tests/test_format_pdf_reader.py
+++ tests/unit_tests/test_format_pdf_reader.py
@@ -7,7 +7,6 @@ from typing import List
 
 import cv2
 from dedocutils.preprocessing import SkewCorrector
-from dedoc.config import get_config
 
 from dedoc.data_structures.line_with_meta import LineWithMeta
 from dedoc.readers.pdf_reader.pdf_image_reader.columns_orientation_classifier.columns_orientation_classifier import ColumnsOrientationClassifier
@@ -144,8 +143,8 @@ class TestPDFReader(unittest.TestCase):
 
     def test_table_extractor(self) -> None:
         config = {
-            'debug_mode': False,
-            'path_debug': '/tmp/dedoc'
+            "debug_mode": False,
+            "path_debug": "/tmp/dedoc"
         }  # TODO: Has to work without config
         any_doc_reader = PdfTxtlayerReader(config=config)
         path = os.path.join(os.path.dirname(__file__), "../data/pdf_with_text_layer/english_doc.pdf")
