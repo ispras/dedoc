c689e6d07ae6cb1685034f3b41bed79b9b913cd2
Belyaeva Oksana <belyaeva@ispras.ru>
2024-03-12 11:42:32 +0300

    TLDR-608 update time benchmarks

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,13 +1,14 @@
-import json
 import os
 import time
 import zipfile
 from collections import OrderedDict, namedtuple
 from tempfile import TemporaryDirectory
+from typing import List
 
 import numpy as np
 import requests
 import wget
+from texttable import Texttable
 from tqdm import tqdm
 
 from dedoc.readers.pdf_reader.pdf_txtlayer_reader.pdfminer_reader.pdfminer_extractor import PdfminerExtractor
@@ -16,7 +17,7 @@ from dedoc.utils.utils import send_file
 path_result = os.path.join(os.path.dirname(__file__), "..", "resources", "benchmarks")
 path_result = os.path.abspath(path_result)
 os.makedirs(path_result, exist_ok=True)
-path_result = os.path.join(path_result, "time_benchmark.json")
+path_result = os.path.join(path_result, "time_benchmark.txt")
 
 Task = namedtuple("Task", ("directory", "name", "parameters", "func_page_count"))
 
@@ -41,6 +42,15 @@ def get_cpu_performance() -> float:
         m2 = np.random.uniform((5000, 5000))
         _ = np.matmul(m1, m2)
     return 1 / (time.time() - start)
+
+
+def get_times(spend_page_times: List, total_size: int, total_time: int, total_files: int, cpu_performance: float = 1.0) -> List[float]:
+    total_time = total_time * cpu_performance
+    throughput = total_size / total_time
+    mean_time_on_file = total_time / total_files
+    mean_time_cpu_on_page = sum(spend_page_times) / len(spend_page_times) * cpu_performance
+
+    return [total_time, throughput, mean_time_on_file, mean_time_cpu_on_page]
 
 
 cpu_performance = get_cpu_performance()
@@ -72,6 +82,16 @@ with TemporaryDirectory() as path_base:
         Task("pdf_tables", "pdf_tables", {}, get_pdf_page_count)
     ]
     print(tasks)
+    table_benchmark = Texttable()
+    header = [
+        ["Dataset", "total_file_size", "total_files", "total_pages", # noqa
+         "|||||||||||||||||||||||||", "|||||||||raw_time||||||", "|||||||||||||||||||||||||", "|||||||||||||||||||||||||",  # noqa
+         "=========================", "====independent_cpu====", "=========================", "========================="],  # noqa
+        ["", "", "", "", "total_time", "throughput", "mean_time_on_file ", "mean_time_cpu_on_page",  # noqa
+         "total_time", "throughput", "mean_time_on_file", "mean_time_cpu_on_page"]]  # noqa
+    table_benchmark.header(header[0])
+    table_benchmark.add_row(header[1])
+    table_benchmark.set_cols_width([25 for _ in header[0]])
     for directory, name, parameters, page_func in tasks:
         total_size = 0
         total_time = 0
@@ -80,6 +100,7 @@ with TemporaryDirectory() as path_base:
         files_info = []
         spend_page_times = []
         directory = os.path.join(path_base, directory)
+
         for file in tqdm(os.listdir(directory), desc=name):
             file_path = os.path.join(directory, file)
             file_size = os.path.getsize(file_path)
@@ -94,21 +115,16 @@ with TemporaryDirectory() as path_base:
             total_pages += pages
             files_info.append({"file": file, "size": file_size, "time_per_file": spend_file_time, "time_per_page": spend_page_times[-1]})
             total_files += 0
-        result_item = OrderedDict()
-        result_item["raw_time"] = total_time
-        result_item["total_time_independent_cpu"] = total_time * cpu_performance
-        result_item["total_file_size"] = total_size
-        result_item["throughput"] = total_size / total_time
-        result_item["mean_time_independent_cpu_on_file"] = total_time / len(files_info) * cpu_performance
-        result_item["mean_time_cpu_on_file"] = total_time / len(files_info)
-        result_item["mean_time_independent_cpu_on_page"] = sum(spend_page_times) / len(spend_page_times) * cpu_performance
-        result_item["mean_time_cpu_on_page"] = sum(spend_page_times) / len(spend_page_times)
-        result_item["total_files"] = len(files_info)
-        result_item["total_pages"] = total_pages
-        result[name] = result_item
 
+        result_item = [name, total_size, len(files_info), total_pages]
+        result_item.extend(get_times(spend_page_times, total_size, total_time, len(files_info)))
+        result_item.extend(get_times(spend_page_times, total_size, total_time, len(files_info), cpu_performance))
+
+        table_benchmark.add_row(result_item)
         with open(path_result, "w") as file_out:
-            json.dump(obj=result, fp=file_out, indent=4, ensure_ascii=False)
+            file_out.write(f"cpu_performance={cpu_performance}")
+            file_out.write("\nTable 1 - benchmarks\n")
+            file_out.write(table_benchmark.draw())
         print(f"save result in {path_result}")
 
     print(failed)
