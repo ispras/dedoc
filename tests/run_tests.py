fb01a9ed74423540e5d5c409042201bed16da6d5
Alexander Golodkov <golodkov@ispras.ru>
2024-05-07 12:52:45 +0300

    fixed style

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -9,8 +9,7 @@ TEST_PREFIX = "test_"
 def run_test_group(group_name: str) -> None:
     runner = CustomTextTestRunner(verbosity=2)
     loader = unittest.TestLoader()
-    test_suite = loader.discover(os.path.join(os.path.dirname(os.path.abspath(__file__)), group_name + "_tests"),
-                                 pattern=TEST_PREFIX + "*.py")
+    test_suite = loader.discover(os.path.join(os.path.dirname(os.path.abspath(__file__)), group_name + "_tests"), pattern=TEST_PREFIX + "*.py")
     runner.run(test_suite)
 
 
