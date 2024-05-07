cceb2e98accdc48e8e9fdc71be1dfeb3d9462174
Alexander Golodkov <golodkov@ispras.ru>
2024-05-07 14:15:36 +0300

    fixed code according to PR comments

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -9,11 +9,10 @@ TEST_PREFIX = "test_"
 def run_test_group(group_name: str) -> None:
     runner = CustomTextTestRunner(verbosity=2)
     loader = unittest.TestLoader()
-    test_suite = loader.discover(os.path.join(os.path.dirname(os.path.abspath(__file__)), group_name + "_tests"), pattern=TEST_PREFIX + "*.py")
+    test_suite = loader.discover(os.path.join(os.path.dirname(os.path.abspath(__file__)), group_name + "_tests"), pattern=f"{TEST_PREFIX}*.py")
     runner.run(test_suite)
 
 
 if __name__ == "__main__":
-    test_groups = ["unit", "api"]
-    for group in test_groups:
+    for group in ("unit", "api"):
         run_test_group(group)
