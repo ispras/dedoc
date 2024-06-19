5117b8f7c8a0f286e55b5991caee6d31b8c844e2
Alexander Golodkov <golodkov@ispras.ru>
2024-06-19 17:47:03 +0300

    more document loader refactoring

---
+++ docs/source/_static/code_examples/langchain/dedoc_pdf_loader.py
@@ -0,0 +1,13 @@
+from dedoc.docs.sourse._static.code_examples.langchain.dedoc_file_loader import DedocFileLoader
+
+
+class DedocPDFLoader(DedocFileLoader):
+    """
+    This loader provides part of the functionality of DedocFileLoader and allows you to load PDF files.
+    To do this, the Dedoc library contains methods for working with documents with and without a text layer.
+    More information is available at the link:
+    https://dedoc.readthedocs.io/en/latest/?badge=latest
+    """
+    def make_config(self) -> dict:
+        from dedoc.utils.langchain import make_manager_pdf_config
+        return make_manager_pdf_config(file_path=self.file_path, parsing_params=self.parsing_params, split=self.split)
