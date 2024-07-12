e057cba1ac4b21524062e32fe86f48afbdd12126
Nasty <bogatenkova.anastasiya@mail.ru>
2024-07-12 16:13:57 +0300

    TLDR-735 some fixes for langchain

--- docs/source/_static/code_examples/langchain/pdf.py
+++ docs/source/_static/code_examples/langchain/pdf.py
@@ -4,20 +4,28 @@ from dedoc_loader import DedocBaseLoader  # noqa from langchain_community.docume
 class DedocPDFLoader(DedocBaseLoader):
     """
     Load PDF files using `dedoc`.
-    The file loader can automatically detect the correctness of a textual layer in the PDF document.
-    Note that `__init__` method supports dedoc_kwargs that differ from ones of DedocBaseLoader.
+    The file loader can automatically detect the correctness of a textual layer in the
+        PDF document.
+    Note that `__init__` method supports dedoc_kwargs that differ from ones of
+        DedocBaseLoader.
 
     dedoc_kwargs: parameters used for document parsing via `dedoc` (https://dedoc.readthedocs.io/en/latest/parameters/parameters.html).
-        with_attachments: enable attached files extraction, you need also need_content_analysis parameter to get results
-        need_content_analysis: enable attached files parsing, works only when with_attachments==True
-        recursion_deep_attachments: recursion level for attached files extraction, works only when with_attachments==True
+        with_attachments: enable attached files extraction, you need also
+            need_content_analysis parameter to get results
+        need_content_analysis: enable attached files parsing, works only when
+            with_attachments==True
+        recursion_deep_attachments: recursion level for attached files extraction,
+            works only when with_attachments==True
         pdf_with_text_layer: type of handler for parsing
         language: language of the document for PDF without a textual layer
         pages: page slice to define the reading range for parsing
-        is_one_column_document: number of columns in case it’s known beforehand for PDF without a textual layer
-        document_orientation: fix document orientation (90, 180, 270 degrees) for PDF without a textual layer
+        is_one_column_document: number of columns in case it’s known beforehand for PDF
+            without a textual layer
+        document_orientation: fix document orientation (90, 180, 270 degrees) for PDF
+            without a textual layer
         need_header_footer_analysis: remove headers and footers from the output result
-        need_binarization: clean pages background (binarize) for PDF without a textual layer
+        need_binarization: clean pages background (binarize) for PDF without a textual
+            layer
         need_pdf_table_analysis: parse tables for PDF without a textual layer
 
     Examples
@@ -36,8 +44,12 @@ class DedocPDFLoader(DedocBaseLoader):
     https://dedoc.readthedocs.io/en/latest/parameters/pdf_handling.html
     https://dedoc.readthedocs.io/en/latest/modules/readers.html#dedoc.readers.PdfAutoReader
     """
-    def _make_config(
-        self
-    ) -> dict:
+
+    def _make_config(self) -> dict:
         from dedoc.utils.langchain import make_manager_pdf_config
-        return make_manager_pdf_config(file_path=self.file_path, parsing_params=self.parsing_parameters, split=self.split)
+
+        return make_manager_pdf_config(
+            file_path=self.file_path,
+            parsing_params=self.parsing_parameters,
+            split=self.split,
+        )
