03dbf9eb6addb09661a4c2320e6f36cea8756232
Alexander Golodkov <golodkov@ispras.ru>
2024-09-11 13:45:07 +0300

    style fix

--- tests/unit_tests/test_module_gost_frame_recognizer.py
+++ tests/unit_tests/test_module_gost_frame_recognizer.py
@@ -6,9 +6,9 @@ import cv2
 import numpy as np
 
 import dedoc.utils.parameter_utils as param_utils
+from dedoc.readers.pdf_reader.pdf_auto_reader.pdf_auto_reader import PdfAutoReader
 from dedoc.readers.pdf_reader.pdf_base_reader import ParametersForParseDoc
 from dedoc.readers.pdf_reader.pdf_image_reader.pdf_image_reader import PdfImageReader
-from dedoc.readers.pdf_reader.pdf_auto_reader.pdf_auto_reader import PdfAutoReader
 from dedoc.readers.pdf_reader.pdf_image_reader.table_recognizer.gost_frame_recognizer import GOSTFrameRecognizer
 from tests.test_utils import get_test_config
 
@@ -86,4 +86,3 @@ class TestGOSTFrameRecognizer(unittest.TestCase):
         self.assertEqual(result.tables[0].cells[0][2].get_text(), "Колонка 3")
         self.assertEqual(len(result.tables[0].cells), 22)
         self.assertTrue("Названне таблицы (продолженне)" in result.lines[0].line)
-
