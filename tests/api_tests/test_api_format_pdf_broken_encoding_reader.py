ce2bf877e7bcb46c40e84ae8a7514029d2be2eb5
sinkudo <mishazagorodnikov@mail.ru>
2025-05-21 15:12:50 +0800

    dockerfile fix, txt path in api test fix

--- tests/api_tests/test_api_format_pdf_broken_encoding_reader.py
+++ tests/api_tests/test_api_format_pdf_broken_encoding_reader.py
@@ -12,7 +12,6 @@ class TestApiPdfBrokenEncodingReader(AbstractTestApiDocReader):
 
     def test_text_extraction(self) -> None:
         file_name = "mongolo.pdf"
-        orig_path = "../data/txt/mongolo.txt"
         result = self._send_request(file_name, dict(pdf_with_text_layer="bad_encoding_reader"))
         tree = result["content"]["structure"]
         text1 = self._get_by_tree_path(tree, "0.0")["text"]
@@ -22,6 +21,6 @@ class TestApiPdfBrokenEncodingReader(AbstractTestApiDocReader):
         text5 = "\n".join(self._get_by_tree_path(tree, "0.1.2.0")["text"].split("\n")[:3])
 
         fulltext = text1 + text2 + text3 + text4 + text5
-        with open(orig_path, encoding="utf8", mode="r") as txt:
+        with open(os.path.join(self.data_directory_path, "txt", "mongolo.txt"), encoding="utf8", mode="r") as txt:
             accuracy = Levenshtein.ratio(txt.read(), fulltext)
             self.assertTrue(accuracy > 0.7)
